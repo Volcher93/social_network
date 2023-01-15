17f23d53706c596eec7b057064d817fa0b49e247
Volcher93 <yz5@bk.ru>
2023-01-15 18:41:24 +0100

    BrowserRouter added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,19 +5,19 @@ function Navbar() {
     return (
         <nav className={p.nav}>
             <div className={`${p.item} ${p.active}`}>
-                <a>Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={p.item}>
-                <a>Messages</a>
+                <a href="/dialogs">Messages</a>
             </div>
             <div className={p.item}>
-                <a>News</a>
+                <a href="/news">News</a>
             </div>
             <div className={p.item}>
-                <a>Music</a>
+                <a href="/music" > Music</a>
             </div>
             <div className={p.item}>
-                <a>Settings</a>
+                <a href="/settings">Settings</a>
             </div>
         </nav>)
 }
