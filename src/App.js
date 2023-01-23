54b5691e573ebab4988e0529556ebd6f5ce80326
Volcher93 <pashkasvr@gmail.com>
2023-01-23 16:07:35 +0100

    Refactoring code.

--- src/App.js
+++ src/App.js
@@ -12,21 +12,19 @@ import Settings from "./components/Settings/Settings";
 function App(props) {
 
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/dialogs'
-                           render={() => <Dialogs state={props.state.messagesPage}/>}/>
-                    <Route path='/profile'
-                           render={() => <Profile state={props.state.profilePage}/>}/>
-                    <Route path='/news' render={() => <News/>}/>
-                    <Route path='/music' render={() => <Music/>}/>
-                    <Route path='/settings' render={() => <Settings/>}/>
-                </div>
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Route path='/dialogs'
+                       render={() => <Dialogs state={props.state.messagesPage}/>}/>
+                <Route path='/profile'
+                       render={() => <Profile state={props.state.profilePage}/>}/>
+                <Route path='/news' render={() => <News/>}/>
+                <Route path='/music' render={() => <Music/>}/>
+                <Route path='/settings' render={() => <Settings/>}/>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
