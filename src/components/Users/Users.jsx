1636a914a99f193c2d0d6bb2042a270cfc84f72e
Volcher93 <pashkasvr@gmail.com>
2023-02-21 19:14:06 +0100

    Added get request for server. Refactoring.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,24 @@
 import React from "react";
 import styles from "./users.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/user.png";
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoURL: 'https://i.pinimg.com/550x/8e/a9/44/8ea944581cb87dd9571b02b9f2d87f15.jpg',
-                    followed: true,
-                    fullName: 'Konstantine',
-                    status: 'like beer',
-                    location: {city: 'Krakow', country: 'Poland'}
-                },
-                {
-                    id: 2,
-                    photoURL: 'https://answers.ea.com/t5/image/serverpage/image-id/59356i97912F434F1FCEFE?v=v2',
-                    followed: false,
-                    fullName: 'Niko',
-                    status: 'playing APEX',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 3,
-                    photoURL: 'https://avatarfiles.alphacoders.com/175/thumb-175704.jpg',
-                    followed: true,
-                    fullName: 'Denis',
-                    status: 'survived 60 days',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                }
-            ]
-        )
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then (response=>{
+            props.setUsers(response.data.items);
+        } );
+
+
+
+
     }
-    console.log(props.users.length);
     return <div>{
         props.users.map(u => <div key={u.id}>
             <div className={styles.user}>
                 <div>
-                    <img src={u.photoURL} className={styles.userPhoto}/>
+                    <img src={u.photos.small !=null ? u.photos.small:userPhoto} className={styles.userPhoto}/>
                 </div>
                 <div className={styles.butt}>
                     {u.followed
@@ -48,10 +30,10 @@ let Users = (props) => {
                         }}>Follow</button>}
                 </div>
                 <div className={styles.userInfo}>
-                    <div className={styles.userName}>{u.fullName}</div>
+                    <div className={styles.userName}>{u.name}</div>
                     <div className={styles.userStatus}>{u.status}</div>
-                    <div className={styles.userCountry}>{u.location.country}</div>
-                    <div className={styles.userCity}>{u.location.city}</div>
+                    <div className={styles.userCountry}>{"u.location.country"}</div>
+                    <div className={styles.userCity}>{"u.location.city"}</div>
                 </div>
             </div>
         </div>)
